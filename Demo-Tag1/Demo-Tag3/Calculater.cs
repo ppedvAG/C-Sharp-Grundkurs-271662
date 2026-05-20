e31e219927fba929f5792d666874e8bc5ae234f7
de2 <de2@WIN11-95>
2026-05-20 09:56:05 +0200

    Funktionen- Tag 3

---
+++ Demo-Tag1/Demo-Tag3/Calculater.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Tag3
+{
+    public static class Calculator
+    {
+        public enum Rechenoperation
+        {
+            Addition = 1,
+            Subtraktion,
+            Multiplikation,
+            Division
+        }
+
+        // public Funktion zum Berechnen, kann von überall verwendet werden.
+        public static double Berechne(double a, double b, Rechenoperation op)
+        {
+            switch (op)
+            {
+                case Rechenoperation.Addition:
+                    return a + b;
+                case Rechenoperation.Subtraktion:
+                    return a - b;
+                case Rechenoperation.Multiplikation:
+                    return a * b;
+                default:
+                    if (b == 0)
+                        return double.NaN;
+                    else
+                        return a / b;
+            }
+        }
+
+        // Funktion überladen
+        public static double Berechne(double a, double b, int op)
+        {
+            return Berechne(a, b, (Rechenoperation)op);
+        }
+
+        public static double Berechne(double a, double b,double c, Rechenoperation op = Rechenoperation.Addition)
+        {
+            switch (op)
+            {
+                case Rechenoperation.Addition:
+                    return a + b+c;
+                case Rechenoperation.Subtraktion:
+                    return a - b-c;
+                case Rechenoperation.Multiplikation:
+                    return a * b*c;
+                default:
+                    if (b == 0 || c == 0)
+                        return double.NaN;
+                    else
+                        return (a / b) / c;
+            }
+        }
+
+        // params array
+        public static double Berechne(Rechenoperation op, params double[] zahlen)
+        {
+            double ergebnis;
+            switch (op)
+            {
+                case Rechenoperation.Addition:
+                    ergebnis = zahlen.Sum();
+                    break;
+                case Rechenoperation.Subtraktion:
+                    ergebnis = zahlen[1];
+                    for(int i = 1; i <zahlen.Length; i++)
+                    {
+                        ergebnis -= zahlen[i];
+                    }
+                    break;
+                case Rechenoperation.Multiplikation:
+                    ergebnis = zahlen[1];
+                    for (int i = 1; i < zahlen.Length; i++)
+                    {
+                        ergebnis *= zahlen[i];
+                    }
+                    break;
+                default:
+                    ergebnis = zahlen[1];
+                    for (int i = 1; i < zahlen.Length; i++)
+                    {
+                        if(zahlen[i] == 0) 
+                            return double.NaN;   
+                        ergebnis = ergebnis / zahlen[i]; // Kurzschreibweise ergebnis /= zahlen[i]
+                    }
+                    break;
+            }
+            return ergebnis;
+        }
+    }
+}
---
+++ Demo-Tag1/Demo-Tag3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Demo_Tag3
+{
+    // Um Calculater. nicht schreiben zu müssen kann man mit using static Klassen einbinden
+    using static Calculator;
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5 * 10 = " + Berechne(5, 10, Rechenoperation.Multiplikation));
+            // Überladene Funktion
+            Console.WriteLine("5 / 10 = " + Berechne(5, 10, 4));
+            Console.WriteLine("5 * 10 * 10 = " + Berechne(5, 10, 10, Rechenoperation.Multiplikation));
+            Console.WriteLine("5 + 10 + 10 = " + Berechne(5, 10, 10.0));
+            // ohne params muss man immer so schreiben
+            double[] zahlen = new double[] {1, 2, 3, 4, 5};
+            Console.WriteLine("Summe Zahlen: "+ Berechne(Rechenoperation.Addition, zahlen));
+
+            // mit params kann ich so schreiben
+            Console.WriteLine("Zahlen multipliziert: " + Berechne(Rechenoperation.Multiplikation, 1, 2, 3, 7, 4, 5, 9, 41, 1, 2, 0));
+        }   
+       
+    }
+}
